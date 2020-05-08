71a1e0a88376672b90f187e5d9a96a1215d80dc8
Jerwin Wieser <j2.wieser@gmail.com>
2020-05-08 16:32:58 +0200

    refined uploading function so that it writes better file names

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='posts-index'),
-    path('upload/', views.uploadcsv, name='posts-upload'),
+    path('upload/', views.upload, name='posts-upload'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -10,6 +10,10 @@ from django.core.files.storage import FileSystemStorage
 import csv, io
 from django.contrib import messages
 
+# get datetime
+from datetime import datetime
+
+
 @login_required(login_url='login')
 def index(request):
 	contents = {
@@ -19,53 +23,23 @@ def index(request):
 
 def upload(request):
 	if request.method == "POST":
+		current_user = request.user
 		uploaded_file = request.FILES['upload_data']
-		fs = FileSystemStorage()
-		fs.save(uploaded_file.name, uploaded_file)
-	return render(request, 'posts/upload.html')
 
-def uploadcsv(request):
-	if request.method == "POST":
-		uploaded_file = request.FILES['upload_data']
+		now = datetime.now()
+		current_date = now.strftime("%Y-%m-%d")
+		current_time = now.strftime("%H:%M:%S")
+		current_datetime = current_date + '_' + current_time
+
+		# create a file name to name the datafile
+		write_file_name = str(current_user) + '_' + uploaded_file.name
+		
 		if not uploaded_file.name.endswith('.csv'):
 			messages.error(request, 'Upload your file as .csv')
 		else: 
 			fs = FileSystemStorage()
-			fs.save(uploaded_file.name, uploaded_file)
+			fs.save(write_file_name, uploaded_file)
 			messages.error(request, 'Your file has been uploaded')
 	return render(request, 'posts/upload.html')
 
 
-
-# Create your views here.
-# # one parameter named request
-# def profile_upload(request):
-#     # declaring template
-#     template = "profile_upload.html"
-#     data = Profile.objects.all()
-# 	# prompt is a context variable that can have different values      depending on their context
-#     prompt = {
-#         'order': 'Order of the CSV should be name, email, address,    phone, profile',
-#         'profiles': data    
-#               }
-#     # GET request returns the value of the data with the specified key.
-#     if request.method == "GET":
-#         return render(request, template, prompt)
-#     csv_file = request.FILES['file']
-#     # let's check if it is a csv file
-#     if not csv_file.name.endswith('.csv'):
-#         messages.error(request, 'THIS IS NOT A CSV FILE')
-#     data_set = csv_file.read().decode('UTF-8')
-#     # setup a stream which is when we loop through each line we are able to handle a data in a stream
-# 	io_string = io.StringIO(data_set)
-# 	next(io_string)
-# 	for column in csv.reader(io_string, delimiter=',', quotechar="|"):
-# 	    _, created = Profile.objects.update_or_create(
-# 	        name=column[0],
-# 	        email=column[1],
-# 	        address=column[2],
-# 	        phone=column[3],
-# 	        profile=column[4]
-# 	    )
-# 	context = {}
-# 	return render(request, template, context)
