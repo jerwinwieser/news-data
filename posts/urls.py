ee480fd1f0b1e05bee1f4606b3e414371f07b25d
Jerwin Wieser <j2.wieser@gmail.com>
2020-05-09 02:03:07 +0200

    started implementing chart.js; still getting some .js function errors

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='posts-index'),
     path('upload/', views.upload, name='posts-upload'),
     path('training/', views.training, name='posts-training'),
+    path('api/data/', views.get_data, name='api-data'),
+ 	path('graph/', views.graph, name='posts-graph'),   
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.core.files.storage import FileSystemStorage
 
 # file upload
-import csv
+import pandas as pd
+import numpy as np
 from django.contrib import messages
 
 # get datetime
@@ -16,6 +17,18 @@ from datetime import datetime
 # to read filenames
 import os
 
+# handle data for charts
+from django.http import JsonResponse
+
+def graph(request, *args, **kwargs):
+	return render(request, 'posts/charts.html', {})
+
+def get_data(request, *args, **kwargs):
+	data = {
+		'sales': 100,
+		'customers': 10,
+	}
+	return JsonResponse(data)
 
 @login_required(login_url='login')
 def index(request):
@@ -23,39 +36,50 @@ def index(request):
 
 @login_required(login_url='login')
 def upload(request):
+	# get user and uploaded file
+	# create some var datetime
+	# create a file name to name the datafile
+	# check whether the uploaded file are as expected
+	# if as expected, save the file in the media folder
+	# add some user feedback with error- or success-messages
 	if request.method == "POST":
-		# get user and uploaded file
 		user = request.user.get_username()
 		file = request.FILES['upload_data']
 
-		# create some var datetime
 		now = datetime.now()
 		date = now.strftime("%Y-%m-%d")
 		time = now.strftime("%H:%M:%S")
 		dt = date + '_' + time
 
-		# create a file name to name the datafile
 		fname = user + '_' + file.name
-		
-		# check whether the uploaded file are as expected
-		# if as expected, save the file in the media folder
-		# add some user feedback with error- or success-messages
+
 		if not fname.endswith('.csv'):
 			messages.error(request, 'No file Uploaded. Upload .csv instead')
 		else: 
 			fs = FileSystemStorage()
 			fs.save(fname, file)
 			messages.success(request, 'Your file has been uploaded')
+
 	return render(request, 'posts/upload.html')
 
 @login_required(login_url='login')
 def training(request):
 	# get the current user
-	user = request.user.get_username()
-
 	# list the file names in the media dir
 	# then get the files from the current user
+	# read in the data file
+	# convert to numpy array
+	# create context to render
+	user = request.user.get_username()
+
 	file_names = os.listdir('media')
 	user_files = [file for file in file_names if str(user) in file]
 
-	return render(request, 'posts/training.html', {'contents': user_files})
+	df = pd.read_csv('media/' + user_files[1])
+	x = np.array(df.PassengerId)
+
+	context = {
+		'filenames': user_files[1],
+		'dataframes': x,
+	}
+	return render(request, 'posts/training.html', context)
